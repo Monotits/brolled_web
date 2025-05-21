80c82383285db8e1835f24a369fe17e99d1afbb9
Buğra Günay <gunaygd@gmail.com>
2025-05-21 20:50:56 +0300

    main.

---
+++ script.js
@@ -0,0 +1,8 @@
+// Future JavaScript code will be added here.
+// For example:
+// - Smooth scrolling
+// - Animations
+// - Form validations
+// - Dynamic content loading
+
+console.log("script.js loaded for Brolled website."); 
